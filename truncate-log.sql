1de1a257da000054ce10029aa7f286dd9de6914c
al2718x <al2718x@gmail.com>
2025-12-08 01:50:42 +0300

    update truncate

--- truncate-log.sql
+++ truncate-log.sql
@@ -2,3 +2,4 @@
 -- DELETE FROM `log` WHERE `updated_at` < (NOW() - INTERVAL 1 WEEK);
 DELETE FROM `log` WHERE `updated_at` < (NOW() - INTERVAL 3 DAY);
 -- DELETE FROM `log` WHERE `updated_at` < (NOW() - INTERVAL 1 DAY);
+ALTER TABLE `log` ROW_FORMAT=COMPRESSED;
